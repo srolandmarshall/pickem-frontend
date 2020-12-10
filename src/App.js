7a59ef0c372259dd937dff59bbba34b9a3c510c5
Sam Marshall <samuel.r.marshall@gmail.com>
2020-12-10 15:11:05 -0500

    added ESPN NFL JSON

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import React, { Component } from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import NotFoundPage from './pages/NotFoundPage';
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 
 export default class App extends Component {
-  render() {
-  return (
-   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={HomePage} exact={true} />
-      <Route path="/search" component={SearchPage} exact={true} />
-      <Route component={NotFoundPage} />
-     </Switch>
-   </BrowserRouter>
-   )
- }
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" component={HomePage} exact={true} />
+					<Route path="/search" component={SearchPage} exact={true} />
+					<Route component={NotFoundPage} />
+				</Switch>
+			</BrowserRouter>
+		);
+	}
 }
